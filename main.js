5181fd166a5c4ff441abc216faca5377a1128303
Adam Cox <adamcox.dev@gmail.com>
2022-09-01 13:12:21 -0600

    Final commit after demo

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const newAgeInput = document.querySelector('#age')
 const newLikesText = document.querySelector('textarea')
 const charContainer = document.querySelector('section')
 
-// const baseURL = 
+const baseURL = 'http://localhost:4000'
 //Step 2 write out the function
 function createCharacterCard(char) {
   let charCard = document.createElement('div')
@@ -34,4 +34,67 @@ function clearCharacters() {
   charContainer.innerHTML = ``
 }
 
+const getAllChars = () => {
+  clearCharacters();
+  axios.get(`${baseURL}/characters`)
+  .then((res) => {
+    console.log(res.data);
+    let characterArray = res.data;
+    for(let i = 0; i < characterArray.length; i++){
+      createCharacterCard(characterArray[i]);
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
+
+const getOneChar = (event) => {
+  clearCharacters();
+  axios.get(`${baseURL}/character/${event.target.id}`)
+  .then((res) => {
+    console.log(res.data)
+    createCharacterCard(res.data)
+    })
+    .catch((error) => {
+    console.log(error)
+    })
+
+}
+
+const addNewChar = (event) => {
+  event.preventDefault()
+
+clearCharacters()
+
+let newLikes = [...newLikesText.value.split(',')]
+  console.log(newLikes)
+
+  let bodyObj = {
+    firstName: newFirstInput.value,
+    lastName: newLastInput.value,
+    gender: newGenderDropDown.value,
+    age: newAgeInput.value,
+    likes: newLikes
+  }
+  axios.post(`${baseURL}/character`, bodyObj)
+  .then((res) => {
+    let newArr = res.data
+    for(let i = 0; i < newArr.length; i++){
+      createCharacterCard(newArr[i]);
+    }
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+}
+
 //Step 3 combine steps 1 and 2 using addEventListener
+
+getAllBtn.addEventListener('click', getAllChars);
+
+for(let i = 0; i < charBtns.length; i++){
+  charBtns[i].addEventListener('click', getOneChar);
+}
+
+createForm.addEventListener('submit', addNewChar);
